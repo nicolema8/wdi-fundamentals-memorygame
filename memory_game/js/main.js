415feb426d0a10d9e35eb76d07e104b302bfd038
Nicole Abellanosa <mikey@nicoles-mbp-2.gateway.sonic.net>
2019-04-15 00:45:14 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,17 +4,23 @@ var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert("Sorry, try again.");
+  console.log("Sorry, try again.");
 }
+}
+
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
